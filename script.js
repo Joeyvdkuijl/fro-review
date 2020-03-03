8012a699211ae5deee2e81b1289a8d50fa85c1fa
Joeyvdkuijl <55228974+Joeyvdkuijl@users.noreply.github.com>
2020-03-03 14:01:51 +0100

    commit 1

---
+++ script.js
@@ -0,0 +1,44 @@
+let status = 0;
+let time = 0;
+
+function start() {
+    status = 1;
+    document.getElementById("startBtn").disabled = true;
+    timer();
+}
+function stop(){
+    status = 0;
+    document.getElementById("startBtn").disabled = false;
+}
+function reset(){
+    status = 0;
+    time = 0;
+    document.getElementById("startBtn").disabled = false;
+    document.getElementById("timerlabel").innerHTML = "00:00:00";
+}
+
+function timer(){
+
+    if(status == 1){
+    setTimeout(function(){
+        time++;
+        var min = Math.floor(time/100/60);
+        var sec = Math.floor(time/100);
+        var mSec = time % 100;
+
+        if(min<10){
+            min = "0" + min;
+        }
+        if(sec>=60){
+            sec = sec % 60;
+        }
+        if(sec<10){
+            sec = "0" + sec;
+        }
+
+        document.getElementById("timerlabel").innerHTML = min + ":" + sec + ":" + mSec;
+        timer();
+
+      }, 10);
+    }
+}
